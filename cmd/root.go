9d31a74162f439e37fe6c5038fbd6b19b11c4eb0
Dongsu Park <dongsu@kinvolk.io>
2018-07-16 16:39:47 +0200

    *: set log level for each logging statement
    Replace each plain log.Print* calls with a logging statement with a
    precise log level, like Info, Warn, Error.

--- cmd/root.go
+++ cmd/root.go
@@ -125,7 +125,7 @@ func runWormholeConnector(cmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 
-	log.Println("Shutting down server...")
+	log.Info("Shutting down server...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
